fcf323199696bbb45fa06d5efc0c5cb18e3b240b
Denys Poltorak <itwfrontend@gmail.com>
2023-10-24 20:39:12 +0300

    feat: add husky and linter (Tue, Oct 24, 2023 08:39:11 PM)

--- src/components/UI/button/Button.tsx
+++ src/components/UI/button/Button.tsx
@@ -16,14 +16,16 @@ export const Button: FC<TButtonProps> = ({
   children,
   disabled,
 }) => {
-  //context
+  // context
   const isPressed = false;
 
   const generalStyles = classNames({
     'button-add': isAdd,
     'button-select': isSelect,
+    // eslint-disable-next-line max-len
     'bg-white !text-green border-green solid !shadow-none dark:bg-dark-surface-2 dark:!text-dark-white dark:border-[transparent] dark:hover:bg-dark-surface-2':
       isPressed && isAdd,
+    // eslint-disable-next-line max-len
     '!text-white bg-primary border-primary dark:bg-dark-white dark:!text-dark-black dark:border-white':
       isPressed && isSelect,
   });
