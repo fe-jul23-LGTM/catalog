cff1a28e6f893f86fada9f10765ef03cfe587d50
Denys Poltorak <itwfrontend@gmail.com>
2023-10-26 13:24:00 +0300

    refactor: fix header styles (Thu, Oct 26, 2023 01:23:59 PM)

--- src/components/widgets/Header/helper.ts
+++ src/components/widgets/Header/helper.ts
@@ -2,6 +2,16 @@ import classNames from 'classnames';
 
 /* eslint-disable max-len */
 export const styleForCurrentLink = ({ isActive }: { isActive: boolean }) =>
-  classNames({
-    'relative text-primary pb-[17px] lg:pb-[24px] after:absolute after:w-full after:h-[3px] after:bg-primary after:bottom-0 after:left-0': isActive,
+  classNames('dark:after:bg-dark-white', {
+    'relative dark:text-dark-white text-primary pb-[17px] lg:pb-[24px] after:absolute after:w-full after:h-[3px] after:bg-primary after:bottom-0 after:left-0':
+      isActive,
   });
+
+export const favoriteNCartStyles = ({ isActive }: { isActive: boolean }) =>
+  classNames(
+    'relative block sm:py-[16px] sm:px-[16px] lg:px-[24px] lg:py-[24px]',
+    {
+      'text-primary pb-[17px] lg:pb-[24px] after:absolute after:left-0 dark:after:bg-dark-white after:right-0 after:h-[3px] after:bg-primary after:bottom-0 after:left-0':
+        isActive,
+    },
+  );
