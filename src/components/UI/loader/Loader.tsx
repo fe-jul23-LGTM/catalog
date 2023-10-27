3d636b68c065201c4aacf41df1c0fa1dedda0df6
irinagos <irynagos47@gmail.com>
2023-10-27 16:01:59 +0300

    feat: add loader (Fri, Oct 27, 2023 04:01:59 PM)

---
+++ src/components/UI/loader/Loader.tsx
@@ -0,0 +1,74 @@
+/* eslint-disable max-len */
+import { FC } from 'react';
+
+export const Loader:FC = () => {
+  return (
+    <div className="bg-gray-200 w-full min-h-screen flex justify-center items-center">
+      <div className="p-4 relative">
+        <svg
+          className="resp-[height/170/70] resp-[width/170/70] animate-spin text-primary dark:text-dark-accent"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M12 4.75V6.25"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></path>
+          <path
+            d="M17.1266 6.87347L16.0659 7.93413"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></path>
+          <path
+            d="M19.25 12L17.75 12"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></path>
+          <path
+            d="M17.1266 17.1265L16.0659 16.0659"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></path>
+          <path
+            d="M12 17.75V19.25"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></path>
+          <path
+            d="M7.9342 16.0659L6.87354 17.1265"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></path>
+          <path
+            d="M6.25 12L4.75 12"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></path>
+          <path
+            d="M7.9342 7.93413L6.87354 6.87347"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></path>
+        </svg>
+      </div>
+    </div>
+  );
+};
