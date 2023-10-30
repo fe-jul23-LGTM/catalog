c6756bdfcce33d2ead5c682348b71b18075c2bc3
Denys Poltorak <itwfrontend@gmail.com>
2023-10-30 10:41:19 +0200

    fix: set page title to localstorage (Mon, Oct 30, 2023 10:41:17 AM)

--- src/helpers/functions.ts
+++ src/helpers/functions.ts
@@ -3,9 +3,11 @@ import { SITE_TITLE } from './constants';
 export const handleTitleChange = (title: string) => {
   if (title) {
     document.title = `${SITE_TITLE} | ${title}`;
+    localStorage.setItem('pageTitle', title);
 
     return;
   }
 
   document.title = SITE_TITLE;
+  localStorage.removeItem('pageTitle');
 };
