4c1f4caa8058ab774320179a4d129b4dd9caf1b8
Ivanna Romanovych <ivankaromanovych12@gmail.com>
2023-11-02 16:33:06 +0200

    fix: add product type (Thu, Nov 02, 2023 04:33:06 PM)

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -4,29 +4,34 @@ import { FC } from 'react';
 import { BannerSlider } from '~components/widgets/BannerSlider';
 import { ProductsSlider } from '~components/widgets/ProductsSlider';
 import { ShopByCategory } from '~components/widgets/ShopByCategory';
+import products from '~public/initial_data/products.json';
 
 type THomeProps = object;
 
-// const phone = {
-//   phoneId: 802390,
-//   phoneName: 'Apple iPhone 11 Pro Max 64GB Gold (iMT9G2FS/A)',
-//   price: 799,
-//   imageSrc: a,
-//   priceWithDiscount: 1000,
-//   screenSize: 6,
-//   typeOfDisplay: 'OLED',
-//   capacity: 128,
-//   ram: 6,
-// };
-
 export const Home: FC<THomeProps> = () => {
+  const hotPricesProducts = [...products].sort(
+    (a, b) => b.fullPrice - b.price - (a.fullPrice - a.price),
+  );
+
+  const brandNewProducts = [...products].sort(
+    (a, b) => b.year - a.year,
+  );
+
   return (
     <>
-      <h1 className='title-1 py-[24px] sm:py-[32px] lg:py-[56px]'>Welcome to Nice Gadgets store!</h1>
+      <h1 className="title-1 py-[24px] sm:py-[32px] lg:py-[56px]">
+        Welcome to Nice Gadgets store!
+      </h1>
       <BannerSlider />
-      <ProductsSlider sliderName='Brand new models' />
+      <ProductsSlider
+        sliderName="Brand new models"
+        productsList={brandNewProducts}
+      />
       <ShopByCategory />
-      <ProductsSlider sliderName='Hot prices' />
+      <ProductsSlider
+        sliderName="Hot prices"
+        productsList={hotPricesProducts}
+      />
     </>
   );
 };
